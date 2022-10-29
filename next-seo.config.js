7ac19135c208fdd54d80fc689f8d6f5e2a459500
Kevin Davis <thekidnamedkd@gmail.com>
2022-10-29 15:16:02 +0100

    build: initial commit

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: "next-chakra-ts",
+  titleTemplate: "%s | nextarter-chakra",
+  defaultTitle: "nextarter-chakra",
+  description: "Next.js + chakra-ui + TypeScript template",
+  canonical: "https://https://lombeira.vercel.app/",
+  openGraph: {
+    url: "https://https://lombeira.vercel.app/",
+    title: "next-chakra-ts",
+    description: "Next.js + chakra-ui + TypeScript template",
+    images: [
+      {
+        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
+        alt: "https://lombeira.vercel.app/ og-image",
+      },
+    ],
+    site_name: "nextarter-chakra",
+  },
+  twitter: {
+    handle: "@Lombeira",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEOConfig;
