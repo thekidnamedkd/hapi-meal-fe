6a376af095fdbbb74d12259a104af45851400891
Kevin Davis <thekidnamedkd@gmail.com>
2022-10-30 02:09:11 +0100

    chore: fix linter errors

--- src/lib/pages/collection/index.tsx
+++ src/lib/pages/collection/index.tsx
@@ -1,48 +1,50 @@
-import { Container, Flex, Img } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Flex, Text, SimpleGrid, Box, Link } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
-import { useEffect, useState } from "react";
-import { Text, SimpleGrid, Box, Link} from '@chakra-ui/react'
-import {ExternalLinkIcon} from '@chakra-ui/icons'
+import Image from "next/image";
 
-import Image from 'next/image'
-import {NFT_MAPPING} from "../../constants";
+import { NFT_MAPPING } from "../../constants";
 
 type CollectionItemProps = {
-    nftItem: string
-}
-const CollectionItem = ({nftItem}: CollectionItemProps) => {
-    return (
-        <Box height="100%" width="100%">
-            <Link href={`/collection/${nftItem}`}>
-                <Image style={{"borderRadius": "15px"}} src={NFT_MAPPING[nftItem]["imgPath"]} objectFit='contain'/>
-            </Link>
-        </Box>
-    )
-}
+  nftItem: string;
+};
+const CollectionItem = ({ nftItem }: CollectionItemProps) => {
+  return (
+    <Box height="100%" width="100%">
+      <Link href={`/collection/${nftItem}`}>
+        <Image
+          style={{ borderRadius: "15px" }}
+          src={NFT_MAPPING[nftItem].imgPath}
+          objectFit="contain"
+        />
+      </Link>
+    </Box>
+  );
+};
 const Collection = () => {
   // TODO: pull NFTs in wallet from database and render here
   return (
     <Flex direction="column" gap={4} mb={8} w="100%">
       <NextSeo title="View Treat" />
-    
+
       <Flex w="full">
-        <Text fontSize="4xl" >Your Collection</Text>  
+        <Text fontSize="4xl">Your Collection</Text>
       </Flex>
 
       <SimpleGrid columns={2} spacing={2.5}>
-        <CollectionItem nftItem="crystal-skologna"/>
-        <CollectionItem nftItem="fry-guy"/>
-        <CollectionItem nftItem="loving-clown-parent"/>
-        <CollectionItem nftItem="master-chef"/>
-        <CollectionItem nftItem="murderous-arnold"/>
-        <CollectionItem nftItem="nose-man-brime-man"/>
+        <CollectionItem nftItem="crystal-skologna" />
+        <CollectionItem nftItem="fry-guy" />
+        <CollectionItem nftItem="loving-clown-parent" />
+        <CollectionItem nftItem="master-chef" />
+        <CollectionItem nftItem="murderous-arnold" />
+        <CollectionItem nftItem="nose-man-brime-man" />
       </SimpleGrid>
 
       <Flex w="full">
-    {/* TODO: hook up end point to export wallet to another wallet */}
-      <Link href='https://chakra-ui.com' isExternal>
-        Export Entire Collection <ExternalLinkIcon mx='2px' />
-      </Link>
+        {/* TODO: hook up end point to export wallet to another wallet */}
+        <Link href="https://chakra-ui.com" isExternal>
+          Export Entire Collection <ExternalLinkIcon mx="2px" />
+        </Link>
       </Flex>
     </Flex>
   );
