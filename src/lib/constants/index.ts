5cd6b8dba9b4cc87486f7c086754ce188f24f1a7
Matthew <mtwichan@gmail.com>
2022-10-30 02:34:34 +0000

    feat: collection item

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -6,10 +6,10 @@ import murderousArnoldIMG from "../../../public/murderous_arnold.jpeg";
 import noseManBrimeManIMG from "../../../public/nose_man_brime_man.png";
 
 export const NFT_MAPPING: any = {
-    "crystal-skologna": {"tokenId": 0 ,"imgPath": crystalSkolognaIMG, "description": ""},
-    "fry-guy": {"tokenId": 1 ,"imgPath": fryGuyIMG},
-    "loving-clown-parent": {"tokenId": 2 ,"imgPath": lovingClownParentIMG},
-    "master-chef": {"tokenId": 3 ,"imgPath": masterChefIMG},
-    "murderous-arnold": {"tokenId": 4 ,"imgPath": murderousArnoldIMG},
-    "nose-man-brime-man": {"tokenId": 5 ,"imgPath": noseManBrimeManIMG},
+    "crystal-skologna": {"tokenId": 0 ,"imgPath": crystalSkolognaIMG, "description": "Nose man haunts you with a single nostril, while brim man lurks in the distance. A satchel bag, or parachute pants, there is no espacing the immaculate style of these masked horrors.", "title": "Crystal Skologna"},
+    "fry-guy": {"tokenId": 1 ,"imgPath": fryGuyIMG, "description": "You gotta bag? Is it secure? Fry guy want’s your bag. Time to pay up. Guppies, shrimps, orca’s, & whales... Fry Guy knows no bounds. Tiny Terror with the sauce & he is ready to dip you to oblivian", "title": "Fry Guy"},
+    "loving-clown-parent": {"tokenId": 2 ,"imgPath": lovingClownParentIMG, "description": "Hi sweaty, i’m so proud of you. I don’t care if you traded to zero by over leveraging your networth with algorithmic stable coins. You can always live in my basement, Muahehehehe", "title": "Loving Clown Parent"},
+    "master-chef": {"tokenId": 3 ,"imgPath": masterChefIMG, "description": "Terminal burger flipper gone Chef Supreme. You better hope Master Chef’s lost eyes don't appear in your take away. He’s always watching, anyway. Chop, chop!", "title": "Master Chef"},
+    "murderous-arnold": {"tokenId": 4 ,"imgPath": murderousArnoldIMG, "description": "This plastic toy is completely harmless as a digital collectible. However, should he appear in your bathroom mirror after you chant “Balmy Arnold” 3 times, there is no hiding from such an absolute creature.", "title": "Murderous Arnold"},
+    "nose-man-brime-man": {"tokenId": 5 ,"imgPath": noseManBrimeManIMG, "description": "Nose man haunts you with a single nostril, while brim man lurks in the distance. A satchel bag, or parachute pants, there is no espacing the immaculate style of these masked horrors.", "title": "Nose Man Brime Man"},
 }
