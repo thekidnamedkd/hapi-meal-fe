c75ecd089111e8e92ae424ec2749e90ca7df16d1
Matthew <mtwichan@gmail.com>
2022-10-30 01:44:59 +0100

    fix: nft mapp constant

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -6,10 +6,10 @@ import murderousArnoldIMG from "../../../public/murderous_arnold.jpeg";
 import noseManBrimeManIMG from "../../../public/nose_man_brime_man.png";
 
 export const NFT_MAPPING: any = {
-    "crystalSkologna": {"tokenId": 0 ,"imgPath": crystalSkolognaIMG},
-    "fryGuy": {"tokenId": 1 ,"imgPath": fryGuyIMG},
-    "lovingClownParent": {"tokenId": 2 ,"imgPath": lovingClownParentIMG},
-    "masterChef": {"tokenId": 3 ,"imgPath": masterChefIMG},
-    "murderousArnold": {"tokenId": 4 ,"imgPath": murderousArnoldIMG},
-    "noseManBrimeMan": {"tokenId": 5 ,"imgPath": noseManBrimeManIMG},
+    "crystal-skologna": {"tokenId": 0 ,"imgPath": crystalSkolognaIMG},
+    "fry-guy": {"tokenId": 1 ,"imgPath": fryGuyIMG},
+    "loving-clown-parent": {"tokenId": 2 ,"imgPath": lovingClownParentIMG},
+    "master-chef": {"tokenId": 3 ,"imgPath": masterChefIMG},
+    "murderous-arnold": {"tokenId": 4 ,"imgPath": murderousArnoldIMG},
+    "nose-man-brime-man": {"tokenId": 5 ,"imgPath": noseManBrimeManIMG},
 }
